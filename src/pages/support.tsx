9eeb4a5d9b0fbe4c03f72146df7c543fc426b80e
Regis Castro <regis.gotardi@gmail.com>
2024-01-12 16:13:59 -0300

    SupportOptions styled component created

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components'
 import { Card } from '~/components/LibraryFilters'
 import Link from 'next/link'
 import Navbar from '~/components/Navbar'
@@ -15,12 +16,12 @@ const Support = () => {
         <p>Se vi taksas nian laboron grava kaj povus subteni nin por daŭrigi la ekziston de ĉi tiu retpaĝo, ni tre ege dankus vin! Por iĝi subtenanto, oni nur bezonas surklaki la ligilon <Link href="https://www.patreon.com/sonlibroj" target="_blank">patreon.com/sonlibroj</Link> kaj krei konton ĉe Patreon. Depende de la monata monsumo ricevita de subtenantoj, eblos al ni ankaŭ plibonigi ĉi tiun retpaĝon estontece.</p>
 
         <h2>Aboneblecoj monate</h2>
-        <div>
-          <p><b>$1</b> - Vi havos aliron al nia Patreon-fluo.</p><br/>
-          <p><b>$5</b> - Vi havos aliron al nia Patreon-fluo kaj via nomo aperos ĉe la malsupra listo de subtenantoj laŭ via permeso.</p><br/>
-          <p><b>$10</b> - Vi havos aliron al nia Patreon-fluo, via nomo aperos ĉe la malsupra listo de subtenantoj laŭ via permeso, kaj vi povas elekti la ĝenron de la legota libro.</p><br/>
+        <SupportOptions>
+          <p><b>$1</b> - Vi havos aliron al nia Patreon-fluo.</p>
+          <p><b>$5</b> - Vi havos aliron al nia Patreon-fluo kaj via nomo aperos ĉe la malsupra listo de subtenantoj laŭ via permeso.</p>
+          <p><b>$10</b> - Vi havos aliron al nia Patreon-fluo, via nomo aperos ĉe la malsupra listo de subtenantoj laŭ via permeso, kaj vi povas elekti la ĝenron de la legota libro.</p>
           <p><b>$30</b> - Vi havos aliron al nia Patreon-fluo, via nomo aperos ĉe la malsupra listo de subtenantoj laŭ via permeso, kaj vi povos elekti legotan libron. Aldone, se vi konsentos, via nomo estos menciita je ĝia PDF-dosiero! &#x1F600;</p>
-        </div>
+        </SupportOptions>
 
         <div className="supporters">
           <h2>Listo de subtenantoj</h2>
@@ -32,5 +33,11 @@ const Support = () => {
     </>
   )
 }
+
+const SupportOptions = styled.div`
+    p:not(:last-child) {
+      margin-bottom: 10px;
+    }
+`
 
 export default Support
